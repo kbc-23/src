f28810bc13ad0825d319d14d0849b3182547ca78
Chowdhury, Kaushik <kaushikbchowdhury@gmail.com>
2022-05-08 22:48:14 +0200

    resolution of boundaries using watershed transformation

--- post_process.py
+++ post_process.py
@@ -37,7 +37,7 @@ plt.imshow(cv2.cvtColor(inp_img,cv2.COLOR_BGR2RGB))
 plt.title(filename_eg)
 plt.axis('off')
 img = inp_img.copy()
-pred_mask = np.zeros([1000,1000],dtype = masks.dtype)
+pred_mask = np.zeros([1000, 1000],dtype = masks.dtype)
 for i in range(len(class_ids)):
     [r1, c1, r2, c2] = boxes[i, :]
     mask_i = masks[i,:,:]
@@ -50,7 +50,38 @@ plt.axis('off')
 
 # %% Overlay
 pred_mask_3D = np.stack((pred_mask,pred_mask,pred_mask),axis=2)
-img_overlay = img *  (pred_mask_3D)
+img_overlay = img * (pred_mask_3D)
 plt.imshow(cv2.cvtColor(img_overlay,cv2.COLOR_BGR2RGB))
+plt.title(filename_eg)
+plt.axis('off')
+
+pred_mask_fullsc = ((pred_mask / np.max(pred_mask))* 255.0).astype(np.uint8)
+
+# threshold
+ret, thresh = cv2.threshold(pred_mask_fullsc,0,255,cv2.THRESH_OTSU)
+# noise removal
+kernel = np.ones((5,5),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
+# background area
+sure_bg = cv2.dilate(opening,kernel,iterations=1)
+# foreground area
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,0)
+n_instances, sure_fg = cv2.threshold(dist_transform,0.3*dist_transform.max(),255,0)
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+# Marker labelling
+n_instances, markers, stats, centroids = cv2.connectedComponentsWithStats(sure_fg)
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers + 1
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+#instances = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [0,255,0]
+
+print('{0} non-overlapping objects found'.format(n_instances))
+
+plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
 plt.title(filename_eg)
 plt.axis('off')
